4dd7d824c25a294bce180b40f8deb5280a81061b
rachelpatricias <230712411@students.uajy.ac.id>
2025-12-05 00:13:32 +0700

    ubah approuter

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,15 +1,17 @@
-// src/routes/AppRouter.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
 // Protected Routes
 import ProtectedAdmin from "./protected/ProtectedAdmin";
 import ProtectedUser from "./protected/ProtectedUser";
 
+// Navbar
+import TopNavbar from "../components/TopNavbar";
+
 // ==== ADMIN PAGES ====
 import AdminDashboardPage from "../pages/admin/AdminDashboardPage";
 import AdminLayananPage from "../pages/admin/AdminLayananPage";
-import AdminPegawaiPage from "../pages/admin/AdminPegawaiPage"; // <-- FIX
+import AdminPegawaiPage from "../pages/admin/AdminPegawaiPage";
 import AdminPesananPage from "../pages/admin/AdminPesananPage";
 
 // ==== USER PAGES ====
@@ -21,34 +23,68 @@ import BookingPage from "../pages/pelanggan/BookingPage";
 import PesananPage from "../pages/pelanggan/PesananPage";
 import PembayaranPage from "../pages/pelanggan/PembayaranPage";
 
-const AppRouter = () => {
+function LayoutRouter() {
+  const location = useLocation();
+  const role = localStorage.getItem("role");
+
+  // Tentukan navbar customer/admin
+  const customerRoutes = [
+    { name: "Home", path: "/home" },
+    { name: "Layanan", path: "/layanan" },
+    { name: "Pesanan", path: "/pesanan" },
+    { name: "Booking", path: "/booking" },
+  ];
+
+  const adminRoutes = [
+    { name: "Dashboard", path: "/admin/dashboard" },
+    { name: "Layanan", path: "/admin/layanan" },
+    { name: "Pegawai", path: "/admin/pegawai" },
+    { name: "Pesanan", path: "/admin/pesanan" },
+  ];
+
+  const navRoutes = role === "admin" ? adminRoutes : customerRoutes;
+
+  // ⛔ HIDE NAVBAR WHEN LOGIN OR REGISTER
+  const hideNavbar = ["/", "/login", "/register"].includes(location.pathname);
+
+  return (
+    <>
+      {/* NAVBAR GLOBAL */}
+      {!hideNavbar && <TopNavbar routes={navRoutes} />}
+
+      <div style={{ paddingTop: hideNavbar ? "0px" : "100px" }}>
+        <Routes>
+
+          {/* AUTH */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+
+          {/* USER ROUTES */}
+          <Route path="/home" element={<ProtectedUser><HomePage /></ProtectedUser>} />
+          <Route path="/layanan" element={<ProtectedUser><LayananPage /></ProtectedUser>} />
+          <Route path="/booking" element={<ProtectedUser><BookingPage /></ProtectedUser>} />
+          <Route path="/pesanan" element={<ProtectedUser><PesananPage /></ProtectedUser>} />
+          <Route path="/pembayaran" element={<ProtectedUser><PembayaranPage /></ProtectedUser>} />
+
+          {/* ADMIN ROUTES */}
+          <Route path="/admin/dashboard" element={<ProtectedAdmin><AdminDashboardPage /></ProtectedAdmin>} />
+          <Route path="/admin/layanan" element={<ProtectedAdmin><AdminLayananPage /></ProtectedAdmin>} />
+          <Route path="/admin/pegawai" element={<ProtectedAdmin><AdminPegawaiPage /></ProtectedAdmin>} />
+          <Route path="/admin/pesanan" element={<ProtectedAdmin><AdminPesananPage /></ProtectedAdmin>} />
+
+          {/* DEFAULT */}
+          <Route path="*" element={<LoginPage />} />
+
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default function AppRouter() {
   return (
     <Router>
-      <Routes>
-
-        {/* LOGIN UNIVERSAL */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-
-        {/* USER PAGES */}
-        <Route path="/home" element={<ProtectedUser><HomePage /></ProtectedUser>} />
-        <Route path="/layanan" element={<ProtectedUser><LayananPage /></ProtectedUser>} />
-        <Route path="/booking" element={<ProtectedUser><BookingPage /></ProtectedUser>} />
-        <Route path="/pesanan" element={<ProtectedUser><PesananPage /></ProtectedUser>} />
-        <Route path="/pembayaran" element={<ProtectedUser><PembayaranPage /></ProtectedUser>} />
-
-        {/* ADMIN PAGES */}
-        <Route path="/admin/dashboard" element={<ProtectedAdmin><AdminDashboardPage /></ProtectedAdmin>} />
-        <Route path="/admin/layanan" element={<ProtectedAdmin><AdminLayananPage /></ProtectedAdmin>} />
-        <Route path="/admin/pegawai" element={<ProtectedAdmin><AdminPegawaiPage /></ProtectedAdmin>} /> {/* <-- FIX */}
-        <Route path="/admin/pesanan" element={<ProtectedAdmin><AdminPesananPage /></ProtectedAdmin>} />
-
-        {/* DEFAULT */}
-        <Route path="*" element={<LoginPage />} />
-
-      </Routes>
+      <LayoutRouter />
     </Router>
   );
-};
-
-export default AppRouter;
+}
